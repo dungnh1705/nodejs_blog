5cf0f0879fcb0f278165cbc63667876736ef0f9f
tieuquai <dung.nh1705@gmail.com>
2022-07-04 14:14:03 +0700

    update refactor code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ const path =  require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded())
+app.use(express.json({
+  extended: true
+}));
 
 // HTTP Logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template
 app.engine('hbs', engine({
@@ -17,9 +23,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
